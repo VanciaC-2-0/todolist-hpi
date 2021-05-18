b915fe7a43ef738633952d684daaf299ed2ad4f0
VanciaC <vancia.chan@ipilyon.net>
2021-05-19 00:01:40 +0200

    Component todoItem, Interfaces  and display some todos

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { useState} from "react";
+import TodoItem from './components/TodoItem';
+import { TodoInterface } from "./interface/TodoInterface";
 
-function App() {
-  return (
-    <div className="App">
-        <h1>Hello world!</h1>
-    </div>
-  );
+const App = () => {
+
+    const [todos, setTodos] = useState<TodoInterface[]>([
+        {"id":"1","createdAt":"2021-05-11T05:39:22.142Z","description":"Slovenia value-added standardization","isCompleted":true},
+        {"id":"2","createdAt":"2021-05-11T04:21:55.747Z","description":"Savings Account","isCompleted":false}
+    ]);
+
+    return (
+        <div className="App">
+            <TodoItem todo={todos[0]} />
+            <TodoItem todo={todos[1]} />
+        </div>
+    );
 }
 
 export default App;
