dc3d84b76436ddfeb4cd12220fde160aa177a3fd
VanciaC <vancia.chan@ipilyon.net>
2021-05-19 19:07:09 +0200

    Add shortid dependency + addTodo functionality

--- src/App.tsx
+++ src/App.tsx
@@ -8,12 +8,22 @@ const App = () => {
     const [todos, setTodos] = useState<TodoInterface[]>([
         {"id":"1","createdAt":"2021-05-11T05:39:22.142Z","description":"Slovenia value-added standardization","isCompleted":true},
         {"id":"2","createdAt":"2021-05-11T04:21:55.747Z","description":"Savings Account","isCompleted":false}
-    ]);
+    ])
+
+    const handleTodoCreate = (todo: TodoInterface) =>{
+        const newTodosState: TodoInterface[] = [...todos]
+        
+        newTodosState.push(todo)
+        
+        setTodos(newTodosState)
+        
+        console.log(newTodosState);
+    }
 
     return (
         <div className="App">
             <TodoItemList todos={todos}/>
-            <TodoForm todos={todos}/>
+            <TodoForm todos={todos} handleTodoCreate={handleTodoCreate}/>
         </div>
     );
 }
