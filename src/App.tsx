2047ffdd502cf063921f689a960156334ad967df
VanciaC <vancia.chan@ipilyon.net>
2021-05-19 20:42:44 +0200

    Delete Todo function

--- src/App.tsx
+++ src/App.tsx
@@ -10,20 +10,24 @@ const App = () => {
         {"id":"2","createdAt":"2021-05-11T04:21:55.747Z","description":"Savings Account","isCompleted":false}
     ])
 
-    const handleTodoCreate = (todo: TodoInterface) =>{
+    const handleTodoAdd = (todo: TodoInterface) =>{
         const newTodosState: TodoInterface[] = [...todos]
         
         newTodosState.push(todo)
         
         setTodos(newTodosState)
-        
-        console.log(newTodosState);
     }
 
+    const handleTodoDelete = (id: String) =>{
+        const newTodosState: TodoInterface[] = todos.filter((todo: TodoInterface) => todo.id !== id)
+
+        setTodos(newTodosState)
+    }
+    
     return (
         <div className="App">
-            <TodoItemList todos={todos}/>
-            <TodoForm todos={todos} handleTodoCreate={handleTodoCreate}/>
+            <TodoItemList todos={todos} handleTodoDelete={handleTodoDelete}/>
+            <TodoForm todos={todos} handleTodoAdd={handleTodoAdd}/>
         </div>
     );
 }
