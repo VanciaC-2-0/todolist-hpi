894b938d30725885e705ae46ae4e970c161b79e2
VanciaC <vancia.chan@ipilyon.net>
2021-05-21 00:10:49 +0200

    Request GET, POST, PUT, DELETE with axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
+import axios from "axios";
 import React, { useEffect, useState} from "react";
 import TodoForm from "./components/TodoForm";
 import TodoItemList from "./components/TodoItemList";
 import { TodoInterface } from "./interface/TodoInterface";
 
 const App = () => {
+    //Endpoint URL
+    const ENDPOINT = "https://609a4b280f5a13001721a85b.mockapi.io/tasks/" 
+
     //State of [todos]
     const [todos, setTodos] = useState<TodoInterface[]>([])
     //Order by createdAt
@@ -20,17 +24,20 @@ const App = () => {
 
     //Collect data from API and put in state
     useEffect(() => {
-        fetch("https://609a4b280f5a13001721a85b.mockapi.io/tasks")
-            .then(res => res.json())
-            .then(
-                todos => setTodos(todos)
-            )
+        axios.get(ENDPOINT)
+            .then(todos => setTodos(todos.data))
     }, [])
     
     //Add todo
     const handleTodoAdd = (todo: TodoInterface) =>{
+        axios.post(ENDPOINT, {
+            id: todo.id,
+            createdAt: todo.createdAt,
+            description: todo.description,
+            isCompleted: todo.isCompleted
+        })
         const newTodosState: TodoInterface[] = [...todos]
-        
+
         newTodosState.push(todo)
         
         setTodos(newTodosState)
@@ -38,6 +45,7 @@ const App = () => {
 
     //Delete one todo
     const handleTodoDelete = (id: String) =>{
+        axios.delete(ENDPOINT+id)
         const newTodosState: TodoInterface[] = todos.filter((todo: TodoInterface) => todo.id !== id)
 
         setTodos(newTodosState)
@@ -51,10 +59,14 @@ const App = () => {
         }
     
     //Completed/notcompleted todo
-    const handleTodoIsComplete = (id: String) =>{
+    const handleTodoIsComplete = (id: String, todo: TodoInterface) =>{
+        axios.put(ENDPOINT+id, {
+            isCompleted: !todo.isCompleted
+        })
+    
         const newTodosState: TodoInterface[] = [...todos]
-
-        newTodosState.find((todo: TodoInterface) => todo.id === id)!.isCompleted = !newTodosState.find((todo: TodoInterface) => todo.id === id)!.isCompleted
+        
+        newTodosState.find(todo => todo.id === id)!.isCompleted = !newTodosState.find((todo: TodoInterface) => todo.id === id)!.isCompleted
 
         setTodos(newTodosState)
     }
